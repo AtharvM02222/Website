8d134f6eb8145cbee912582d8d1a41c1a918a9fc
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2024-11-12 09:21:24 +0530

    Update work.js

--- Index/work.js
+++ Index/work.js
@@ -1,1 +1,10 @@
 
+const workBox = document.getElementById("workBox");
+
+workBox.addEventListener("mouseenter", () => {
+  workBox.style.borderImage = "linear-gradient(45deg, red, orange, yellow, green, blue, indigo, violet) 1";
+});
+
+workBox.addEventListener("mouseleave", () => {
+  workBox.style.borderImage = "none";
+});
