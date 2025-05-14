6c0a6cdee364f027799fc97238fe669676c67dc3
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2025-05-14 10:18:38 +0530

    Update cloud.js

--- Index/cloud.js
+++ Index/cloud.js
@@ -1,16 +1,1 @@
-// cloud.js
 
-// 1) YOUR FIREBASE CONFIG
-const firebaseConfig = {
-  apiKey: "AIzaSyDW9a-Dd4c44QRsjUSpNcjvn1RPzOorXw4",
-  authDomain: "protean-tooling-444907-k3.firebaseapp.com",
-  projectId: "protean-tooling-444907-k3",
-  storageBucket: "protean-tooling-444907-k3.firebasestorage.app",
-  messagingSenderId: "989275453863",
-  appId: "1:989275453863:web:f04a2937a7785c75231e82",
-  measurementId: "G-6ZE618L262"
-};
-
-
-// Ensure Netlify Identity is initialized
-netlifyIdentity.init();
